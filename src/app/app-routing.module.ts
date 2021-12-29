b3caaf221b2261008a4bf590463a820f322538a3
Marek Wilewski <m.a.wilewski@gmail.com>
2021-12-29 11:16:47 +0100

    Commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DecisionButtonsComponent } from './decision-buttons/decision-buttons.component';
+import { ProfileBrowsingComponent } from './profile-browsing/profile-browsing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ProfileBrowsingComponent},
+  {path:'profil', component: DecisionButtonsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
